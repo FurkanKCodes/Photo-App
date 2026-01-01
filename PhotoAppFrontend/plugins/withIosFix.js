8d41b8b88d1d4d244ac04f4487c042a9382f3645
FurkanKCodes <furkankosen22@gmail.com>
2026-01-01 18:45:52 +0300

    Update iOS fix to patch Podfile instead of Xcode project

--- PhotoAppFrontend/plugins/withIosFix.js
+++ PhotoAppFrontend/plugins/withIosFix.js
@@ -1,17 +1,36 @@
-const { withXcodeProject } = require('@expo/config-plugins');
+const { withPodfile } = require('@expo/config-plugins');
 
 module.exports = function withIosFix(config) {
-  return withXcodeProject(config, (config) => {
-    const xcodeProject = config.modResults;
-    const configurations = xcodeProject.pbxXCBuildConfigurationSection();
-    
-    for (const key in configurations) {
-      const buildSettings = configurations[key].buildSettings;
-      if (buildSettings) {
-        // Bu ayar, "include of non-modular header" hatasını "YES" diyerek kabul eder ve hatayı çözer
-        buildSettings.CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES = 'YES';
-      }
+  return withPodfile(config, (config) => {
+    const targetCode = `
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+      end
+    end`;
+
+    const podfileContents = config.modResults.contents;
+
+    // Eğer kod daha önce eklenmişse tekrar ekleme (çakışmayı önle)
+    if (podfileContents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES')) {
+      return config;
     }
+
+    // Expo'nun mevcut 'post_install' bloğunu bul ve kodumuzu içine enjekte et
+    if (podfileContents.includes('post_install do |installer|')) {
+      config.modResults.contents = podfileContents.replace(
+        'post_install do |installer|',
+        `post_install do |installer|${targetCode}`
+      );
+    } else {
+      // Eğer blok yoksa (nadir durum), yeni bir blok oluştur
+      config.modResults.contents += `
+        post_install do |installer|
+          ${targetCode}
+        end
+      `;
+    }
+
     return config;
   });
 };
